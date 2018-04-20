e12ba6802d09f45811ec551a944a7eb85cc02d9f
Dan Irwin <daniel.irwin@enernoc.com>
2018-04-19 21:39:48 -0400

    fix git ignore, and conditional in Tracer

---
+++ index.js
@@ -0,0 +1,8 @@
+
+module.exports.UUID = require('./lib/UUID');
+module.exports.Tracer = require('./lib/Tracer');
+module.exports.Har = require('./lib/Har');
+module.exports.Entry = require('./lib/Entry');
+module.exports.HyperRequestHarBuilder = require('./lib/HyperRequestHarBuilder');
+module.exports.Auditor = require('./lib/Auditor');
+module.exports.Client = require('./lib/Client');
